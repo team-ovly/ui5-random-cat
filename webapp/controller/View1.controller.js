f88883f5671a2457110c6a2df63033fa3afcdcdd
Fábio Luiz Esperati Pagoti <fabiopagoti@gmail.com>
2019-09-02 18:19:12 +0000

    detail page is updated when adding an item or clicking on one

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,42 +1,84 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/StandardListItem"
-], function (Controller, StandardListItem) {
+	"sap/m/StandardListItem",
+	"sap/m/ListType"
+], function (Controller, StandardListItem, ListType) {
 	"use strict";
 
 	return Controller.extend("ovly.random-cat.controller.View1", {
-		
+
+		/**
+		 * Attributes
+		 */
 		endpoint: "https://aws.random.cat/meow",
-		
+
 		counter: 1,
-		
-		onInit: function(){
+
+		/**
+		 * Hook methods
+		 */
+		onInit: function () {
 			// @type sap.m.List
 			this._list = this.byId("cat-list");
+			this._detailPage = this.byId("detail-page");
+			this._image = this.byId("cat-image");
 		},
-		
+
+		/**
+		 * Event Handlers
+		 */
 		onEmpty: function (oEvent) {
 			this.counter = 1;
 			this._list.removeAllItems();
 		},
-		
-		onAdd: function(oEvent){
+
+		onAdd: function (oEvent) {
 			this._callAPI();
 		},
-		
-		_callAPI: function(){
-			$.get(this.endpoint, function(oResponse){
-				this.addCatInsideList(oResponse.file);
+
+		onPressItem: function (oEvent) {
+			var oItem = oEvent.getSource();
+			this._updateDetailPage({
+				id: oItem.getTitle(),
+				url: oItem.getIcon()
+			});
+		},
+
+		/**
+		 * Auxiliar methods
+		 */
+		_callAPI: function () {
+			$.get(this.endpoint, function (oResponse) {
+				this._addCatInsideList(oResponse.file);
 			}.bind(this));
 		},
-		
-		addCatInsideList: function(sUrl){
+
+		_addCatInsideList: function (sUrl) {
 			var oNewItem = new StandardListItem({
 				title: this.counter++,
-				icon: sUrl
+				icon: sUrl,
+				type: ListType.Navigation,
+				press: [this.onPressItem, this] // bind
 			});
-			
+
 			this._list.insertItem(oNewItem, 0);
+
+			this._updateDetailPage({
+				id: oNewItem.getTitle(),
+				url: oNewItem.getIcon()
+			});
+
+		},
+
+		/**
+		 * @param {object} oCat 
+		 * @param {string} oCat.id - History Id
+		 * @param {string} oCat.url - Cat URL
+		 */
+		_updateDetailPage: function (oCat) {
+			this._detailPage.setTitle(oCat.id);
+			this._image.setSrc(oCat.url);
 		}
+
 	});
 });
